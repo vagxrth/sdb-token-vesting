e30b48f83ab25b8588c740585d4acc0d5409abf8
Vagarth <vagarth419@gmail.com>
2024-11-25 01:32:03 +0530

    feat: create vesting account struct

--- anchor/programs/vesting/src/lib.rs
+++ anchor/programs/vesting/src/lib.rs
@@ -8,6 +8,31 @@ declare_id!("AsjZ3kWAUSQRNt2pZVeJkywhZ6gpLpHZmJjduPmKZDZZ");
 pub mod vesting {
     use super::*;
 
-    
+    pub fn createVestingAccount(ctx: Context<CreateVestingAccount>) -> Result<()> {
 
+      Ok(())
+    }
+
+}
+
+
+#[derive(Accounts)]
+pub struct CreateVestingAccount<'info> {
+  #[account(mut)]
+  pub signer: Signer<'info>
+
+  #[account(
+    init,
+    space = 8 + 
+  )]
+}
+
+#[account]
+pub struct VestingAccount {
+  pub owner: Pubkey,
+  pub mint: Pubkey,
+  pub cold_token_account: Pubkey,
+  pub company_name: String,
+  pub treasury_bump: u8,
+  pub bump: u8
 }
