f5809ef7a4c12e46a6df1d65ed239b8a2e0bae14
Vagarth <vagarth419@gmail.com>
2024-11-25 01:12:02 +0530

    chore: initial commit

---
+++ src/components/vesting/vesting-data-access.tsx
@@ -0,0 +1,104 @@
+'use client'
+
+import {getVestingProgram, getVestingProgramId} from '@project/anchor'
+import {useConnection} from '@solana/wallet-adapter-react'
+import {Cluster, Keypair, PublicKey} from '@solana/web3.js'
+import {useMutation, useQuery} from '@tanstack/react-query'
+import {useMemo} from 'react'
+import toast from 'react-hot-toast'
+import {useCluster} from '../cluster/cluster-data-access'
+import {useAnchorProvider} from '../solana/solana-provider'
+import {useTransactionToast} from '../ui/ui-layout'
+
+export function useVestingProgram() {
+  const { connection } = useConnection()
+  const { cluster } = useCluster()
+  const transactionToast = useTransactionToast()
+  const provider = useAnchorProvider()
+  const programId = useMemo(() => getVestingProgramId(cluster.network as Cluster), [cluster])
+  const program = getVestingProgram(provider)
+
+  const accounts = useQuery({
+    queryKey: ['vesting', 'all', { cluster }],
+    queryFn: () => program.account.vesting.all(),
+  })
+
+  const getProgramAccount = useQuery({
+    queryKey: ['get-program-account', { cluster }],
+    queryFn: () => connection.getParsedAccountInfo(programId),
+  })
+
+  const initialize = useMutation({
+    mutationKey: ['vesting', 'initialize', { cluster }],
+    mutationFn: (keypair: Keypair) =>
+      program.methods.initialize().accounts({ vesting: keypair.publicKey }).signers([keypair]).rpc(),
+    onSuccess: (signature) => {
+      transactionToast(signature)
+      return accounts.refetch()
+    },
+    onError: () => toast.error('Failed to initialize account'),
+  })
+
+  return {
+    program,
+    programId,
+    accounts,
+    getProgramAccount,
+    initialize,
+  }
+}
+
+export function useVestingProgramAccount({ account }: { account: PublicKey }) {
+  const { cluster } = useCluster()
+  const transactionToast = useTransactionToast()
+  const { program, accounts } = useVestingProgram()
+
+  const accountQuery = useQuery({
+    queryKey: ['vesting', 'fetch', { cluster, account }],
+    queryFn: () => program.account.vesting.fetch(account),
+  })
+
+  const closeMutation = useMutation({
+    mutationKey: ['vesting', 'close', { cluster, account }],
+    mutationFn: () => program.methods.close().accounts({ vesting: account }).rpc(),
+    onSuccess: (tx) => {
+      transactionToast(tx)
+      return accounts.refetch()
+    },
+  })
+
+  const decrementMutation = useMutation({
+    mutationKey: ['vesting', 'decrement', { cluster, account }],
+    mutationFn: () => program.methods.decrement().accounts({ vesting: account }).rpc(),
+    onSuccess: (tx) => {
+      transactionToast(tx)
+      return accountQuery.refetch()
+    },
+  })
+
+  const incrementMutation = useMutation({
+    mutationKey: ['vesting', 'increment', { cluster, account }],
+    mutationFn: () => program.methods.increment().accounts({ vesting: account }).rpc(),
+    onSuccess: (tx) => {
+      transactionToast(tx)
+      return accountQuery.refetch()
+    },
+  })
+
+  const setMutation = useMutation({
+    mutationKey: ['vesting', 'set', { cluster, account }],
+    mutationFn: (value: number) => program.methods.set(value).accounts({ vesting: account }).rpc(),
+    onSuccess: (tx) => {
+      transactionToast(tx)
+      return accountQuery.refetch()
+    },
+  })
+
+  return {
+    accountQuery,
+    closeMutation,
+    decrementMutation,
+    incrementMutation,
+    setMutation,
+  }
+}
