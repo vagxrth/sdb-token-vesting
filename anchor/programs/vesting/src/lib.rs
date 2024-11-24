4924ec1c92c6003db0ea3cabe2c6c3ab4bb78d26
Vagarth <vagarth419@gmail.com>
2024-11-25 02:36:03 +0530

    feat: create vesting and cold token account

--- anchor/programs/vesting/src/lib.rs
+++ anchor/programs/vesting/src/lib.rs
@@ -1,6 +1,7 @@
 #![allow(clippy::result_large_err)]
 
 use anchor_lang::prelude::*;
+use anchor_spl::token_interface::{Mint, TokenAccount, TokenInterface};
 
 declare_id!("AsjZ3kWAUSQRNt2pZVeJkywhZ6gpLpHZmJjduPmKZDZZ");
 
@@ -8,7 +9,7 @@ declare_id!("AsjZ3kWAUSQRNt2pZVeJkywhZ6gpLpHZmJjduPmKZDZZ");
 pub mod vesting {
     use super::*;
 
-    pub fn createVestingAccount(ctx: Context<CreateVestingAccount>) -> Result<()> {
+    pub fn createVestingAccount(ctx: Context<CreateVestingAccount>, company_name: String) -> Result<()> {
 
       Ok(())
     }
@@ -17,21 +18,46 @@ pub mod vesting {
 
 
 #[derive(Accounts)]
+#[instruction(company_name: String)]
 pub struct CreateVestingAccount<'info> {
   #[account(mut)]
-  pub signer: Signer<'info>
+  pub signer: Signer<'info>,
 
   #[account(
     init,
-    space = 8 + 
+    space = 8 + VestingAccount::INIT_SPACE,
+    payer = signer,
+    seeds = [company_name.as_ref()],
+    bump
   )]
+
+  pub vesting_account: Account<'info, VestingAccount>,
+
+  pub mint: InterfaceAccount<'info, Mint>,
+
+  #[account(
+    init,
+    token::mint = mint,
+    token::authority = cold_token_account,
+    payer = signer,
+    seeds = [b"vesting_cold", company_name.as_bytes()],
+    bump
+  )]
+
+  pub cold_token_account: InterfaceAccount<'info, TokenAccount>,
+
+  pub system_program: Program<'info, System>,
+  pub token_program: Interface<'info, TokenInterface>
+
 }
 
 #[account]
+#[derive(InitSpace)]
 pub struct VestingAccount {
   pub owner: Pubkey,
   pub mint: Pubkey,
   pub cold_token_account: Pubkey,
+  #[max_len(50)]
   pub company_name: String,
   pub treasury_bump: u8,
   pub bump: u8
