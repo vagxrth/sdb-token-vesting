97550cec8127913929e8933c18dcc1754b5b89ce
Vagarth <vagarth419@gmail.com>
2024-11-28 02:20:09 +0530

    feat: modify vesting data access component

--- src/components/vesting/vesting-data-access.tsx
+++ src/components/vesting/vesting-data-access.tsx
@@ -1,38 +1,48 @@
-'use client'
+"use client";
 
-import {getVestingProgram, getVestingProgramId} from '@project/anchor'
-import {useConnection} from '@solana/wallet-adapter-react'
-import {Cluster, Keypair, PublicKey} from '@solana/web3.js'
-import {useMutation, useQuery} from '@tanstack/react-query'
-import {useMemo} from 'react'
-import toast from 'react-hot-toast'
-import {useCluster} from '../cluster/cluster-data-access'
-import {useAnchorProvider} from '../solana/solana-provider'
-import {useTransactionToast} from '../ui/ui-layout'
-import { TOKEN_PROGRAM_ID } from '@solana/spl-token'
+import { getVestingProgram, getVestingProgramId } from "@token-vesting/anchor";
+import { useConnection } from "@solana/wallet-adapter-react";
+import { Cluster, PublicKey } from "@solana/web3.js";
+import { useMutation, useQuery } from "@tanstack/react-query";
+import { useMemo } from "react";
+import toast from "react-hot-toast";
+import { useCluster } from "../cluster/cluster-data-access";
+import { useAnchorProvider } from "../solana/solana-provider";
+import { useTransactionToast } from "../ui/ui-layout";
+import { TOKEN_PROGRAM_ID } from "@solana/spl-token";
 
 interface CreateVestingArgs {
   companyName: string;
   mint: string;
 }
 
+interface CreateEmployeeArgs {
+  startTime: number;
+  endTime: number;
+  totalAmount: number;
+  cliffTime: number;
+}
+
 export function useVestingProgram() {
-  const { connection } = useConnection()
-  const { cluster } = useCluster()
-  const transactionToast = useTransactionToast()
-  const provider = useAnchorProvider()
-  const programId = useMemo(() => getVestingProgramId(cluster.network as Cluster), [cluster])
-  const program = getVestingProgram(provider)
+  const { connection } = useConnection();
+  const { cluster } = useCluster();
+  const transactionToast = useTransactionToast();
+  const provider = useAnchorProvider();
+  const programId = useMemo(
+    () => getVestingProgramId(cluster.network as Cluster),
+    [cluster]
+  );
+  const program = getVestingProgram(provider);
 
   const accounts = useQuery({
-    queryKey: ['vesting', 'all', { cluster }],
+    queryKey: ["vesting", "all", { cluster }],
     queryFn: () => program.account.vestingAccount.all(),
-  })
+  });
 
   const getProgramAccount = useQuery({
-    queryKey: ['get-program-account', { cluster }],
+    queryKey: ["get-program-account", { cluster }],
     queryFn: () => connection.getParsedAccountInfo(programId),
-  })
+  });
 
   const createVestingAccount = useMutation<string, Error, CreateVestingArgs>({
     mutationKey: ["vestingAccount", "create", { cluster }],
@@ -58,56 +68,29 @@ export function useVestingProgram() {
 }
 
 export function useVestingProgramAccount({ account }: { account: PublicKey }) {
-  const { cluster } = useCluster()
-  const transactionToast = useTransactionToast()
-  const { program, accounts } = useVestingProgram()
+  const { cluster } = useCluster();
+  const transactionToast = useTransactionToast();
+  const { program, accounts } = useVestingProgram();
 
   const accountQuery = useQuery({
-    queryKey: ['vesting', 'fetch', { cluster, account }],
-    queryFn: () => program.account.vesting.fetch(account),
-  })
+    queryKey: ["vesting", "fetch", { cluster, account }],
+    queryFn: () => program.account.vestingAccount.fetch(account),
+  });
 
-  const closeMutation = useMutation({
-    mutationKey: ['vesting', 'close', { cluster, account }],
-    mutationFn: () => program.methods.close().accounts({ vesting: account }).rpc(),
+  const createEmployeeVesting = useMutation<string, Error, CreateEmployeeArgs>({
+    mutationKey: ["vesting", "close", { cluster, account }],
+    mutationFn: ({ startTime, endTime, totalAmount, cliffTime }) =>
+      program.methods
+        .createEmployeeVesting(startTime, endTime, totalAmount, cliffTime)
+        .rpc(),
     onSuccess: (tx) => {
-      transactionToast(tx)
-      return accounts.refetch()
+      transactionToast(tx);
+      return accounts.refetch();
     },
-  })
-
-  const decrementMutation = useMutation({
-    mutationKey: ['vesting', 'decrement', { cluster, account }],
-    mutationFn: () => program.methods.decrement().accounts({ vesting: account }).rpc(),
-    onSuccess: (tx) => {
-      transactionToast(tx)
-      return accountQuery.refetch()
-    },
-  })
-
-  const incrementMutation = useMutation({
-    mutationKey: ['vesting', 'increment', { cluster, account }],
-    mutationFn: () => program.methods.increment().accounts({ vesting: account }).rpc(),
-    onSuccess: (tx) => {
-      transactionToast(tx)
-      return accountQuery.refetch()
-    },
-  })
-
-  const setMutation = useMutation({
-    mutationKey: ['vesting', 'set', { cluster, account }],
-    mutationFn: (value: number) => program.methods.set(value).accounts({ vesting: account }).rpc(),
-    onSuccess: (tx) => {
-      transactionToast(tx)
-      return accountQuery.refetch()
-    },
-  })
+  });
 
   return {
     accountQuery,
-    closeMutation,
-    decrementMutation,
-    incrementMutation,
-    setMutation,
-  }
+    createEmployeeVesting,
+  };
 }
